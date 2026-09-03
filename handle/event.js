43674725b2b246c76f15808e7cbbd3bbb26a2b31
david-stack-source <bruhcuy388@gmail.com>
2026-09-03 10:45:31 +0700

    Update file

--- handle/event.js
+++ handle/event.js
@@ -29,10 +29,10 @@ export async function handleMessage(ctx, m, store, plugins) {
         //...set.owner
     ].map(x => x.replace(/[^0-9]/g, "") + "@s.whatsapp.net").includes(m?.sender),
     isBot = m.fromMe || false;
-
+    
     // Filter Mode Bot (Self / Public)
     const mode = (conf.mode || 'public').toLowerCase();
-
+ 
     // Mode Self
     if (mode === 'self' && !isCreator && !isBot) return;
 
